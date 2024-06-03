d02cc33fc9d3fcb91851063fa34aeb9d3c5a8f17
TNOK_AKUSHIROYO <qqnq7um9k@gmail.com>
2024-06-03 13:00:00 +0900

    added Test 3

---
+++ Test3/Test3/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+    Private Sub Button_test_Click(sender As Object, e As EventArgs) Handles Button_test.Click
+        Dim kosuu As Integer
+        'kosuu = 0
+
+        'Do While kosuu < 20
+        '   Debug.Write("◎")
+        '   kosuu = kosuu + 1
+        'Loop
+
+        For kosuu = 0 To 19
+            Debug.Write("◎")
+        Next kosuu
+    End Sub
+End Class
