530f26753f9265cd1411ef49db28bbc0dd308bda
TNOK_AKUSHIROYO <qqnq7um9k@gmail.com>
2024-06-03 12:00:00 +0900

    added Test4

---
+++ Test4/Test4/Form1.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button_test = New Button()
+        SuspendLayout()
+        ' 
+        ' Button_test
+        ' 
+        Button_test.Location = New Point(343, 222)
+        Button_test.Name = "Button_test"
+        Button_test.Size = New Size(131, 40)
+        Button_test.TabIndex = 0
+        Button_test.Text = "Button1"
+        Button_test.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(12F, 30F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Button_test)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents Button_test As Button
+
+End Class
---
+++ Test4/Test4/Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+    Private Sub Button_test_Click(sender As Object, e As EventArgs) Handles Button_test.Click
+        Dim hairetsu(9) As Char
+        Dim kaisuu As Integer
+
+        For kaisuu = 0 To 9
+            hairetsu(kaisuu) = "◎"
+        Next kaisuu
+
+        Debug.Print(CStr(hairetsu))
+    End Sub
+End Class
