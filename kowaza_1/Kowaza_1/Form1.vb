afb03a56a235a7112cca527bee9a04ee4ca87143
TNOK_AKUSHIROYO <qqnq7um9k@gmail.com>
2024-06-13 12:00:00 +0900

    added kowaza1

---
+++ kowaza_1/Kowaza_1/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form_main
+    Private Sub Button_JP_Click(sender As Object, e As EventArgs) Handles Button_JP.Click
+        Dim form As New Form_JP
+        form.Show()
+    End Sub
+
+    Private Sub Button_Eng_Click(sender As Object, e As EventArgs) Handles Button_Eng.Click
+        Dim form As New Form_Eng
+        form.Show()
+    End Sub
+End Class
