d2d156a7e1449379f928d78f300694c3dc519201
TNOK_AKUSHIROYO <qqnq7um9k@gmail.com>
2024-06-20 12:00:00 +0900

    added Test13

---
+++ Test13/Test13/Form1.Designer.vb
@@ -0,0 +1,79 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        MenuStrip1 = New MenuStrip()
+        ファイルToolStripMenuItem = New ToolStripMenuItem()
+        開くToolStripMenuItem = New ToolStripMenuItem()
+        OpenFileDialog1 = New OpenFileDialog()
+        MenuStrip1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' MenuStrip1
+        ' 
+        MenuStrip1.ImageScalingSize = New Size(28, 28)
+        MenuStrip1.Items.AddRange(New ToolStripItem() {ファイルToolStripMenuItem})
+        MenuStrip1.Location = New Point(0, 0)
+        MenuStrip1.Name = "MenuStrip1"
+        MenuStrip1.Size = New Size(800, 38)
+        MenuStrip1.TabIndex = 0
+        MenuStrip1.Text = "MenuStrip1"
+        ' 
+        ' ファイルToolStripMenuItem
+        ' 
+        ファイルToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {開くToolStripMenuItem})
+        ファイルToolStripMenuItem.Name = "ファイルToolStripMenuItem"
+        ファイルToolStripMenuItem.Size = New Size(93, 34)
+        ファイルToolStripMenuItem.Text = "ファイル"
+        ' 
+        ' 開くToolStripMenuItem
+        ' 
+        開くToolStripMenuItem.Name = "開くToolStripMenuItem"
+        開くToolStripMenuItem.Size = New Size(315, 40)
+        開くToolStripMenuItem.Text = "コピー"
+        ' 
+        ' OpenFileDialog1
+        ' 
+        OpenFileDialog1.FileName = "OpenFileDialog1"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(12F, 30F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(MenuStrip1)
+        MainMenuStrip = MenuStrip1
+        Name = "Form1"
+        Text = "Form1"
+        MenuStrip1.ResumeLayout(False)
+        MenuStrip1.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents ファイルToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents 開くToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+
+End Class
---
+++ Test13/Test13/Form1.vb
@@ -0,0 +1,26 @@
+﻿Public Class Form1
+    Private Sub 開くToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles 開くToolStripMenuItem.Click
+        Dim yomikomi_file As String
+        Dim yomikomi As IO.StreamReader
+        Dim yomikomi_gyou As String
+        Dim kakikomi As IO.StreamWriter
+        Dim x_y(1), kakikomi_gyou As String
+
+        If OpenFileDialog1.ShowDialog() = DialogResult.OK Then
+            yomikomi_file = OpenFileDialog1.FileName
+            kakikomi = New IO.StreamWriter("コピー.txt")
+            yomikomi = New IO.StreamReader(yomikomi_file)
+            yomikomi_gyou = yomikomi.ReadLine
+
+            Do While IsNothing(yomikomi_gyou) = False
+                x_y = yomikomi_gyou.Split
+                kakikomi_gyou = x_y(0) & " " & x_y(1)
+                kakikomi.WriteLine(kakikomi_gyou)
+                yomikomi_gyou = yomikomi.ReadLine
+            Loop
+
+            yomikomi.Close()
+            kakikomi.Close()
+        End If
+    End Sub
+End Class
