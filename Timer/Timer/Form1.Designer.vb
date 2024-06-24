905b19cbb3e113cfa472c8bc2a1b022a670cc23a
TNOK_AKUSHIROYO <qqnq7um9k@gmail.com>
2024-06-24 10:20:25 +0900

    added Timer

---
+++ Timer/Timer/Form1.Designer.vb
@@ -0,0 +1,144 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        Label1 = New Label()
+        Button_start = New Button()
+        Button_stop = New Button()
+        Button_reset = New Button()
+        Timer1 = New System.Windows.Forms.Timer(components)
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 30F)
+        Label1.Location = New Point(186, 37)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(78, 93)
+        Label1.TabIndex = 0
+        Label1.Text = "0"
+        ' 
+        ' Button_start
+        ' 
+        Button_start.Location = New Point(160, 188)
+        Button_start.Name = "Button_start"
+        Button_start.Size = New Size(131, 40)
+        Button_start.TabIndex = 1
+        Button_start.Text = "START"
+        Button_start.UseVisualStyleBackColor = True
+        ' 
+        ' Button_stop
+        ' 
+        Button_stop.Location = New Point(525, 188)
+        Button_stop.Name = "Button_stop"
+        Button_stop.Size = New Size(131, 40)
+        Button_stop.TabIndex = 2
+        Button_stop.Text = "STOP"
+        Button_stop.UseVisualStyleBackColor = True
+        ' 
+        ' Button_reset
+        ' 
+        Button_reset.Location = New Point(160, 344)
+        Button_reset.Name = "Button_reset"
+        Button_reset.Size = New Size(131, 40)
+        Button_reset.TabIndex = 3
+        Button_reset.Text = "RESET"
+        Button_reset.UseVisualStyleBackColor = True
+        ' 
+        ' Timer1
+        ' 
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 30F)
+        Label2.Location = New Point(364, 37)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(78, 93)
+        Label2.TabIndex = 4
+        Label2.Text = "0"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI", 30F)
+        Label3.Location = New Point(546, 37)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(78, 93)
+        Label3.TabIndex = 5
+        Label3.Text = "0"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(270, 100)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(35, 30)
+        Label4.TabIndex = 6
+        Label4.Text = "分"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(473, 100)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(35, 30)
+        Label5.TabIndex = 7
+        Label5.Text = "秒"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(12F, 30F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Button_reset)
+        Controls.Add(Button_stop)
+        Controls.Add(Button_start)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button_start As Button
+    Friend WithEvents Button_stop As Button
+    Friend WithEvents Button_reset As Button
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+
+End Class
---
+++ Timer/Timer/Form1.vb
@@ -0,0 +1,31 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button_start.Click
+        Timer1.Enabled = True
+    End Sub
+
+    Private Sub Button_stop_Click(sender As Object, e As EventArgs) Handles Button_stop.Click
+        Timer1.Enabled = False
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Timer1.Interval = 100
+        Label3.Text = Val(Label3.Text) + 1
+
+        If (Label3.Text = 10) Then
+            Label2.Text = Label2.Text + 1
+            Label3.Text = 0
+        End If
+
+        If (Label2.Text = 60) Then
+            Label1.Text = Label1.Text + 1
+            Label2.Text = 0
+        End If
+    End Sub
+
+    Private Sub Button_reset_Click(sender As Object, e As EventArgs) Handles Button_reset.Click
+        Label1.Text = 0
+        Label2.Text = 0
+        Label3.Text = 0
+    End Sub
+
+End Class
